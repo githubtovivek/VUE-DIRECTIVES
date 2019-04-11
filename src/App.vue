fec1d470e954a99bc8e13e2b5ab94b974c45578d
vivek <vsingh135@sapient.com>
2019-04-11 18:25:40 +0100

    custom directives

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives Exercise</h1>
+                <h1 v-vueAppOn:click="executeMe" :style="{color: changeColor}">Directives Exercise</h1>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
 
@@ -12,7 +12,28 @@
 </template>
 
 <script>
-    export default {}
+    export default {
+        data: function() {
+            return {
+                changeColor: 'red'
+            }
+        },
+        directives: {
+            vueAppOn: {
+                bind(el, bindings, node) {
+                    let eventName = bindings.arg;
+                    let fn = bindings.value;
+
+                    el.addEventListener(eventName, fn);
+                }
+            }
+        },
+        methods: {
+            executeMe() {
+                this.changeColor = 'green'
+            }
+        }
+    }
 </script>
 
 <style>
